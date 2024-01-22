2714a50224852f46193a5017d1c1c619a4f345a9
Jinseong <hdss8234@gmail.com>
2024-01-22 20:26:34 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/kr/ac/primitive/PrimitiveApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.primitive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimitiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimitiveApplication.class, args);
+    }
+
+}

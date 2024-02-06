a41080c7a7eb5a8bb1a3a3d63aeabee4d602e45a
Jinseong <hdss8234@gmail.com>
2024-02-06 18:14:08 +0900

    feat: Service 개발

--- src/main/java/kr/ac/primitive/service/PostService.java
+++ src/main/java/kr/ac/primitive/service/PostService.java
@@ -3,6 +3,7 @@ package kr.ac.primitive.service;
 import kr.ac.primitive.dto.PostDto;
 import kr.ac.primitive.entity.Post;
 import kr.ac.primitive.repository.PostRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
+@Slf4j
 public class PostService {
 
     @Autowired
@@ -27,20 +29,31 @@ public class PostService {
 
     @Transactional
     public Post createPost(PostDto postDto) {
-        return null;
+        Post post = postDto.toEntity();
+        if(post.getId() != null) {
+            return null;
+        }
+        return postRepository.save(post);
     }
 
     @Transactional
     public Post updatePost(Long id, PostDto postDto){
-        return null;
+        Post post = postDto.toEntity();
+        Post target = postRepository.findById(id).orElse(null);
+        if(target == null || id != post.getId()){
+            log.info("target : {}, id : {}, post.getId() : {}", target, id, post.getId());
+            return null;
+        }
+        target.update(post);
+        return postRepository.save(target);
     }
 
     @Transactional
     public Post deletePost(Long id){
-        // 1. 대상 찾기
         Post target = postRepository.findById(id).orElse(null);
-        if(target == null) return null;
-        // 2. 대상 삭제
+        if(target == null) {
+            return null;
+        }
         postRepository.delete(target);
         return target;
     }

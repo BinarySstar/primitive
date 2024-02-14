033ecd02b740d6af7216a88a0f82f5a5730296d9
Jinseong <hdss8234@gmail.com>
2024-02-14 21:35:10 +0900

    fix: 스프링 세션 이용

--- src/main/java/kr/ac/primitive/user/HomeController.java
+++ src/main/java/kr/ac/primitive/user/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,15 +23,12 @@ public class HomeController {
         return "home";
     }*/
 
+    /**
+     * 스프링의 @SessionAttribute를 이용하여 세션을 찾고, 세션에 들어있는 데이터를 찾는 번거로운 과정을 한번에 편리하게 처리
+     */
     @GetMapping("/")
-    public String homeLogin (HttpServletRequest request, Model model) {
+    public String homeLogin (@SessionAttribute(name=SessionConst.LOGIN_USER, required = false) User loginUser, Model model) {
         //세션이 없으면 home
-        HttpSession session = request.getSession(false);
-        if (session == null) {
-            return "home";
-        }
-        //로그인
-        User loginUser = (User)session.getAttribute(SessionConst.LOGIN_USER);
         if (loginUser == null) {
             return "home";
         }

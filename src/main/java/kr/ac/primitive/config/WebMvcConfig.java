5a30a80bde662596988a420175fdce014dde9ee7
Jinseong <hdss8234@gmail.com>
2024-03-03 23:46:18 +0900

    feat: 스프링 CORS 해결

--- src/main/java/kr/ac/primitive/config/WebMvcConfig.java
+++ src/main/java/kr/ac/primitive/config/WebMvcConfig.java
@@ -1,2 +1,21 @@
-package kr.ac.primitive.config;public class WebMvcConfig {
+package kr.ac.primitive.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    private final long MAX_AGES_SECS = 3600;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 모든 경로에 대해
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGES_SECS);
+    }
 }

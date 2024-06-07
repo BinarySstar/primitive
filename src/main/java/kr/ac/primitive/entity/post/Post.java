e75d80eccb2493a8df55b02a90aa427cffbfc296
Jinseong <hdss8234@gmail.com>
2024-06-07 16:27:33 +0900

    feat: post 엔티티 추가

---
+++ src/main/java/kr/ac/primitive/entity/post/Post.java
@@ -0,0 +1,57 @@
+package kr.ac.primitive.entity.post;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter // id는 변하면 안됨
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+    private String summary;
+    private String description;
+
+    @Column(nullable = false)
+    private boolean isPublic;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    @Getter // user는 변하면 안됨
+    private User user;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<TechStack> techStacks;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Participant> participants;
+
+    public Post() {
+    }
+
+    public Post(String title, String summary, String description, boolean isPublic, String image, LocalDateTime createdAt, User user, List<TechStack> techStacks, List<Participant> participants) {
+        this.title = title;
+        this.summary = summary;
+        this.description = description;
+        this.isPublic = isPublic;
+        this.image = image;
+        this.createdAt = createdAt;
+        this.user = user;
+        this.techStacks = techStacks;
+        this.participants = participants;
+    }
+}
--- src/main/java/kr/ac/primitive/entity/post/PostRepository.java
+++ src/main/java/kr/ac/primitive/entity/post/PostRepository.java
@@ -1,7 +1,8 @@
-package kr.ac.primitive.repository;
+package kr.ac.primitive.entity.post;
 
-import kr.ac.primitive.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 }

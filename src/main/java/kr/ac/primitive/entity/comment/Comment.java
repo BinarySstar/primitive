cead7c336d09a92e1fb0bc81ffca8084fb72a150
Jinseong <hdss8234@gmail.com>
2024-06-20 22:09:58 +0900

    new : Comment 엔티티 구현

--- src/main/java/kr/ac/primitive/entity/comment/Comment.java
+++ src/main/java/kr/ac/primitive/entity/comment/Comment.java
@@ -1,2 +1,39 @@
-package kr.ac.primitive.entity.comment;public class Comment {
+package kr.ac.primitive.entity.comment;
+
+import jakarta.persistence.*;
+import kr.ac.primitive.entity.post.Post;
+import kr.ac.primitive.entity.user.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    private LocalDateTime createdAt;
+
+    @JoinColumn(name = "post_id")
+    @ManyToOne
+    private Post post;
+
+    @JoinColumn(name = "users_id")
+    @ManyToOne
+    private User user;
+
+    @Builder
+    public Comment(String content, LocalDateTime createdAt, Post post, User user) {
+        this.content = content;
+        this.createdAt = createdAt;
+        this.post = post;
+        this.user = user;
+    }
 }
--- src/main/java/kr/ac/primitive/entity/comment/CommentRepository.java
+++ src/main/java/kr/ac/primitive/entity/comment/CommentRepository.java
@@ -1,2 +1,9 @@
-package kr.ac.primitive.entity.comment;public interface CommentRepository {
+package kr.ac.primitive.entity.comment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
 }

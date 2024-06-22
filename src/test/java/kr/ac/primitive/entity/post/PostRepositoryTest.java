eae2e046d89887bf842e9cededbc04ae9cdd3d28
Jinseong <hdss8234@gmail.com>
2024-06-22 16:13:21 +0900

    refactor : PostRepositoryTest 리팩토링

--- src/test/java/kr/ac/primitive/entity/post/PostRepositoryTest.java
+++ src/test/java/kr/ac/primitive/entity/post/PostRepositoryTest.java
@@ -1,15 +1,16 @@
 package kr.ac.primitive.entity.post;
 
 import kr.ac.primitive.dto.post.request.PostRequestDto;
-import kr.ac.primitive.entity.user.Role;
-import kr.ac.primitive.entity.user.User;
-import kr.ac.primitive.entity.user.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,145 +19,108 @@ class PostRepositoryTest {
 
     @Autowired
     private PostRepository postRepository;
+    private List<Post> posts;
+    private final Random random = new Random();
 
-    @Autowired
-    private UserRepository userRepository;
+    @BeforeEach
+    void setup() {
+        posts = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            posts.add(createPost(i));
+        }
+        postRepository.saveAll(posts);
+    }
+
+    private Post createPost(int i) {
+        return new Post.Builder()
+                .title("title" + i)
+                .summary("summary" + i)
+                .description("description" + i)
+                .image("image" + i)
+                .isPublic(true)
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
 
     @Test
-    @DisplayName("Post가 DB에 저장되는지 테스트")
+    @DisplayName("Post 저장 테스트")
     void savePost() {
         // given
-        User user = new User("name", "studentId", "password", Role.USER);
-        userRepository.save(user);
-
-        Post post = new Post.Builder()
-                .title("title")
-                .summary("summary")
-                .description("description")
-                .isPublic(true)
-                .user(user)
-                .build();
+        Post post = getRandomPost();
 
         // when
         Post savedPost = postRepository.save(post);
 
         // then
+        assertNotNull(savedPost);
+        assertNotNull(savedPost.getId());
         assertEquals(post, savedPost);
+    }
 
-        Post foundPost = postRepository.findById(savedPost.getId()).orElse(null);
+    @Test
+    @DisplayName("모든 Post 조회")
+    void findAllPosts() {
+        // when
+        List<Post> allPosts = postRepository.findAll();
+
+        // then
+        assertEquals(posts.size(), allPosts.size());
+    }
+
+    @Test
+    @DisplayName("특정 Post 조회")
+    void getPost() {
+        // given
+        Post post = getRandomPost();
+
+        // when
+        Post foundPost = postRepository.findById(post.getId()).orElse(null);
+
+        // then
         assertNotNull(foundPost);
         assertEquals(post, foundPost);
-        assertEquals(savedPost, foundPost);
     }
 
     @Test
-    @DisplayName("Post 여러 개를 한 번에 조회")
-    void getAllPost() {
-        //given
-        User user = new User("name", "studentId", "password", Role.USER);
-        userRepository.save(user);
-
-        Post post1 = new Post.Builder()
-                .title("title1")
-                .summary("summary1")
-                .description("description1")
-                .isPublic(true)
-                .user(user)
-                .techStacks(null)
-                .build();
-
-        Post post2 = new Post.Builder()
-                .title("title2")
-                .summary("summary2")
-                .description("description2")
-                .isPublic(true)
-                .user(user)
-                .image("image")
-                .build();
-
-        Post post3 = new Post.Builder()
-                .title("title3")
-                .summary("summary3")
-                .description("description3")
-                .isPublic(true)
-                .user(user)
-                .participants(null)
-                .build();
-
-        postRepository.save(post1);
-        postRepository.save(post2);
-        postRepository.save(post3);
-
-        //when
-        List<Post> posts = postRepository.findAll();
-
-        //then
-        assertEquals(3, posts.size());
-    }
-    @Test
-    @DisplayName("Post가 DB에서 삭제되는지 테스트")
-    void deletePost(){
-        // given
-        User user = new User("name", "studentId", "password", Role.USER);
-        userRepository.save(user);
-
-        Post post = new Post.Builder()
-                .title("title")
-                .summary("summary")
-                .description("description")
-                .isPublic(true)
-                .user(user)
-                .build();
-
-        Post savedPost = postRepository.save(post);
-
-        // when
-        postRepository.delete(savedPost);
-
-        // then
-        Post foundPost = postRepository.findById(savedPost.getId()).orElse(null);
-        assertNull(foundPost);
-    }
-
-    @Test
-    @DisplayName("PostReqeustDto를 통해 Post가 수정되는지 테스트")
+    @DisplayName("Post 수정")
     void updatePost() {
         // given
-        User user = new User("name", "studentId", "password", Role.USER);
-        userRepository.save(user);
-
-        Post post = new Post.Builder()
-                .title("title")
-                .summary("summary")
-                .description("description")
-                .isPublic(true)
-                .user(user)
+        Post post = getRandomPost();
+        postRepository.save(post);
+        PostRequestDto updateRequest = new PostRequestDto.Builder()
+                .title("updated title")
+                .summary("updated summary")
+                .description("updated description")
+                .image("updated image")
+                .isPublic(false)
                 .build();
-
-        Post savedPost = postRepository.save(post);
 
         // when
-        // 저장된 post를 불러오고 PostReqeustDto를 통해 수정
-        PostRequestDto postRequestDto = new PostRequestDto.Builder()
-                .title("new title")
-                .summary("new summary")
-                .description("new description")
-                .image("new image")
-                .isPublic(false)
-                .techStacks(null)
-                .participants(null)
-                .build();
-        Post updatedPost = postRepository.findById(savedPost.getId()).orElse(null);
-        updatedPost.update(postRequestDto);
+        post.update(updateRequest);
+        Post updatedPost = postRepository.save(post);
 
         // then
-        assertNotNull(postRequestDto);
-        assertEquals("new title", updatedPost.getTitle());
-        assertEquals("new summary", updatedPost.getSummary());
-        assertEquals("new description", updatedPost.getDescription());
-        assertEquals("new image", updatedPost.getImage());
-        assertFalse(updatedPost.isPublic());
-        assertNull(updatedPost.getTechStacks());
-        assertNull(updatedPost.getParticipants());
+        Post foundPost = postRepository.findById(post.getId()).orElse(null);
+        assertNotNull(foundPost);
+        assertEquals(updatedPost, foundPost);
+    }
+
+    @Test
+    @DisplayName("Post 삭제")
+    void deletePost() {
+        // given
+        Post post = getRandomPost();
+        postRepository.save(post);
+
+        // when
+        postRepository.delete(post);
+
+        // then
+        Post foundPost = postRepository.findById(post.getId()).orElse(null);
+        assertNull(foundPost, "삭제된 Post");
+    }
+
+    private Post getRandomPost() {
+        return posts.get(random.nextInt(posts.size()));
     }
 }

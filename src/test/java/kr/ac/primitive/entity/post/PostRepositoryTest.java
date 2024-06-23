6df6c49b9ab4100647395618bd932d1394386ff2
Jinseong <hdss8234@gmail.com>
2024-06-23 12:57:39 +0900

    refactor : PostRepository 테스트 작성

--- src/test/java/kr/ac/primitive/entity/post/PostRepositoryTest.java
+++ src/test/java/kr/ac/primitive/entity/post/PostRepositoryTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -38,7 +37,6 @@ class PostRepositoryTest {
                 .description("description" + i)
                 .image("image" + i)
                 .isPublic(true)
-                .createdAt(LocalDateTime.now())
                 .build();
     }
 

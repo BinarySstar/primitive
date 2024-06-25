6e61c1d8d929b267d7744d8e1a119f2946515bf5
Jinseong <hdss8234@gmail.com>
2024-06-26 00:33:20 +0900

    chore : 코드 정리

--- src/main/java/kr/ac/primitive/entity/member/MemberRepository.java
+++ src/main/java/kr/ac/primitive/entity/member/MemberRepository.java
@@ -6,6 +6,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByEmail(String email);
-
     boolean existsByStudentId(String studentId);
 }

80379338ed4d0901db2a51411c93c5f37b30d1cd
Jinseong <hdss8234@gmail.com>
2024-06-29 23:47:15 +0900

    feat : UserDetails 구현

---
+++ src/main/java/kr/ac/primitive/entity/member/MemberSecurity.java
@@ -0,0 +1,51 @@
+package kr.ac.primitive.entity.member;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class MemberSecurity implements UserDetails {
+    private final Member member;
+
+    public MemberSecurity(Member member) {
+        this.member = member;
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getEmail();
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+
+}
